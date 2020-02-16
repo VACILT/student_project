3dbae4e2b19b93f9088d8f60dd8ca8b7093860b6
Willy Stöckel <wilsto2@web.de>
2020-02-16 17:32:43 +0100

    finished reading in all variables

--- multi_variable_plot.py
+++ multi_variable_plot.py
@@ -13,39 +13,31 @@
 #     name: python3
 # ---
 
-import numpy as np
+import xarray as xr
+from pathlib import Path
+import glob
 import holoviews as hv
 from holoviews import opts
-from holoviews import streams
-import xarray as xr
 import hvplot.xarray
-import glob
-#from scipy.stats import combine_pvalues
-#hv.extension('bokeh')
-#import scipy.stats as st
-#import panel as pn
 
-#
+# +
+infiles_period = sorted(glob.glob(f'zmnoy_full/*1960-2099*.nc'))
+infiles_new = sorted(glob.glob(f'zmnoy_full/*2011-2099*.nc'))
+infiles_old = sorted(glob.glob(f'zmnoy_full/*1960-2010*.nc'))
 
-def combine_by_period(var):
-    new = '2011-2099'
-    old ='1960-2010'
-    old_data = sorted(glob.glob(f'zmnoy_full/{var}*{old}*.nc'))
-    new_data = sorted(glob.glob(f'zmnoy_full/{var}*{new}*.nc'))
-    ds_old = xr.open_mfdataset(old_data, concat_dim='ens', combine = 'nested')
-    ds_new = xr.open_mfdataset(new_data, concat_dim='ens', combine = 'nested')
-    ds = xr.concat([ds_old, ds_new], 'period')
-    ds['period'] =[old,new]
-    return(ds)
+cascade_infiles = [[infiles_period[0:4], infiles_period[4:8],infiles_period[8:12],infiles_period[12:16]], \
+[infiles_new[0:4], infiles_new[4:8],infiles_new[8:12],infiles_new[12:16]], \
+[infiles_old[0:4], infiles_old[4:8],infiles_old[8:12],infiles_old[12:16]]]
 
+ds = xr.open_mfdataset(cascade_infiles, concat_dim=['per','var','ens'], combine = 'nested')
+ds['ens'] = ['WACCM_r1', 'WACCM_r2', 'WACCM_r3', 'SOCOL']
+ds['var'] = ['zmnoy','zmo3','zmta','zmua']
+ds['per'] = ['1960-2099','2011-2099','1960-2010']
+# -
 
-ds_zmnoy = combine_by_period('zmnoy')
-ds_zmo3  = combine_by_period('zmo3')
-ds_zmta  = combine_by_period('zmta')
-ds_zmua  = combine_by_period('zmua')
-ds = xr.concat([ds_zmnoy, ds_zmo3,ds_zmta, ds_zmua], 'vari')
-ds['vari'] =['zmnoy','zmo3','zmta','zmua']
+ds
+
 
 co_opts = dict(logy = True, cmap = 'RdBu_r', symmetric=True, colorbar = True, tools = ['hover'], frame_width = 300)
-coefs = ds['coefs'].hvplot.quadmesh('lat','plev',**co_opts).redim.range(coefs=(-1.0,1.0)).opts(opts.QuadMesh(title='Model', invert_yaxis=True))
-
+coefs = ds['coefs'].hvplot.quadmesh('lat','plev',**co_opts).opts(opts.QuadMesh(title='Model', invert_yaxis=True))
+coefs
