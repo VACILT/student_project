542d6ed233223dee7c1f13a10ed80b94dd1eeebe
Willy Stöckel <wilsto2@web.de>
2020-03-31 16:13:15 +0200

    connected widgets

--- Combined.py
+++ Combined.py
@@ -67,7 +67,7 @@ ens_ls = ['WACCM_r1', 'WACCM_r2', 'WACCM_r3', 'SOCOL']
 month_names = ['January', 'February','March','April','May','June','July','Sep','Oct','Nov','Dec']
 ds_sel['ens'] = range(4)
 ds_sel['month'] = np.arange(1,13,1)
-ds_sel['per']
+ds_sel
 
 
 # ## define Tap-plot
@@ -94,12 +94,21 @@ def create_taps_graph(x, y):
 
 
 # +
+# creating widgets
+month_selec =  pn.widgets.IntSlider(name='Month', value=1, start=1, end=12)
 
-#titel as html
-hv_div = hv.Div(f"""<h2>{sel_var} response to {sel_reg} for {month_names[sel_month-1]}</h2>""")
+ens_selec   =  pn.widgets.IntSlider(name='Ens', value=1, start=0, end=3)
+
+reg_selec   =  pn.widgets.Select(name='Regressor', options=['CO2EQ', 'EESC', 'ENSO', 'QBO30', 'QBO50', 'eep_for_noy_and_o3',
+       'epp_for_t_and_u', 'f107', 'intercept', 'spe_for_noy_and_o3',
+       'spe_for_t_and_u'])
+
+per_selec   =  pn.widgets.RadioBoxGroup(name='Period', options=['1960-2099', '2011-2099', '1960-2010'], inline=True)
+
+
 #creating Quadmesh
 graph_opts = dict(cmap = 'RdBu_r', symmetric=True, logy = True, colorbar = True, \
-                ylim=(1000,0.1), active_tools=['pan'])
+                ylim=(1000,0.1), active_tools=['pan'],title='Tap to compare ens')
 
 graph = ds_sel['coefs'].hvplot.quadmesh(x = 'x', y = 'y' ).opts(**graph_opts)
 
@@ -112,18 +121,15 @@ taps_graph = hv.DynamicMap(
                 create_taps_graph,
                 streams=[tap_stream])
 
-# creating widgets
-month_selec = pn.widgets.IntSlider(name='Month', value=1, start=1, end=12)
-reg_selec=pn.widgets.Select(name='Regressor', options=['CO2EQ', 'EESC', 'ENSO', 'QBO30', 'QBO50', 'eep_for_noy_and_o3',
-       'epp_for_t_and_u', 'f107', 'intercept', 'spe_for_noy_and_o3',
-       'spe_for_t_and_u'])
-per_selec= pn.widgets.RadioBoxGroup(name='Period', options=['1960-2099', '2011-2099', '1960-2010'], inline=True)
-
 @pn.depends(stream.param.x, stream.param.y, month_sel=month_selec.param.value,
-            reg_sel=reg_selec.param.value, per_sel=per_selec.param.value)
-def location(x, y, month_sel, reg_sel, per_sel):
+            reg_sel=reg_selec.param.value, per_sel=per_selec.param.value, ens_sel=ens_selec.param.value)
+def location(x, y, month_sel, reg_sel, per_sel, ens_sel):
 
-    first_column = pn.pane.Str(f'Click at {x:.2f}, {y:.2f}, month = {month_sel:.0f} , reg = {reg_sel} , per = {per_sel}')
+    first_column = pn.pane.Str(f'Click at {x:.2f}, {y:.2f}, month = {month_sel:.0f} , reg = {reg_sel} , per = {per_sel}, ens ={ens_sel}')
+    hv_panel[1][0][0].value=per_sel
+    hv_panel[1][0][1].value=ens_sel
+    hv_panel[1][0][2].value=reg_sel
+    hv_panel[1][0][3].value=month_sel
     if np.nan not in [x,y]:
 
 
@@ -133,23 +139,17 @@ def location(x, y, month_sel, reg_sel, per_sel):
         second_column = temp['coefs'].hvplot(width = 400) * temp2.hvplot.scatter(c='k')
     else:
         second_column = pn.Spacer(name='Series Graph')
-    return pn.Column(first_column, second_column, month_selec, reg_selec, per_selec)
+    return pn.Column(first_column, second_column, month_selec, reg_selec, per_selec, ens_selec)
     
 #  adding panel to gain control over widgets
 hv_panel = pn.panel(graph*taps_graph)
 hv_panel.pprint()
-#month_slider = hv_panel[1][0][1]
-widgets =hv_panel[1]
 
 # +
-
 gspec = pn.GridSpec(width=800, height=600)
-gspec[0, 0] = hv.Div("""<h2 align="center">Tap-plot</h2>""")
-gspec[0, 1] = hv_div
-gspec[1:4, 0] = hv_panel[0]
-gspec[1:3, 1] = location
-gspec[4, 0] = (widgets)
-    
+gspec[0, 0] = hv_panel[0]
+gspec[0, 1] = location
+   
 gspec
 # -
 
