ba7d7cffe283f7e5acfb6b8284a4baee6765a924
Willy Stöckel <wilsto2@web.de>
2020-04-01 16:05:16 +0200

    update Combined

--- Combined.py
+++ Combined.py
@@ -48,35 +48,22 @@ ds['var'] = ['zmnoy','zmo3','zmta','zmua']
 ds['per'] = ['1960-2099','2011-2099','1960-2010']
 # -
 
-# ## select data here ...
-
-sel_reg = 'f107'     #'CO2EQ', 'EESC', 'ENSO', 'QBO30', 'QBO50', 'eep_for_noy_and_o3',
-                     #'epp_for_t_and_u', 'f107', 'intercept', 'spe_for_noy_and_o3',
-                     #'spe_for_t_and_u'
-sel_month = 1 
-sel_per ='1960-2099' #'1960-2099','2011-2099','1960-2010'
-sel_var = 'zmnoy'    #'zmnoy','zmo3','zmta','zmua'
-plot_var = '1'       # 0 for Tapplot, 1 for Roi-drawer
-
 # ## rearange/ rename dataset
 
-sel_dict = dict(var=sel_var)
-ds_sel = ds.sel(**sel_dict).rename({'lat': 'x', 'plev': 'y'})
+ds_sel = ds.rename({'lat': 'x', 'plev': 'y'})
 ds_sel['coefs'].attrs['units'] = '%'
 ens_ls = ['WACCM_r1', 'WACCM_r2', 'WACCM_r3', 'SOCOL']
-month_names = ['January', 'February','March','April','May','June','July','Sep','Oct','Nov','Dec']
+month_names = ['January', 'February','March','April','May','June','July','August','September','October','November','December']
+ens_names = ['WACCM_r1', 'WACCM_r2', 'WACCM_r3', 'SOCOL']
 ds_sel['ens'] = range(4)
 ds_sel['month'] = np.arange(1,13,1)
-ds_sel
-
+#ds_sel['var']
 
 # ## define Tap-plot
 # We used the holoviews streams Tap-function to track taps on the quadmesh plot. Out of that, we created a curve comparing the different models on one certain location.
 
 def create_taps_graph(x, y):
-    """
-    from: https://github.com/intake/xrviz/blob/master/xrviz/dashboard.py#L369
-    """
+
     colors = ['#60fffc', '#6da252', '#ff60d4', '#ff9400', '#f4e322',
                   '#229cf4', '#af9862', '#629baf', '#7eed5a', '#e29ec8',
                   '#ff4300']
@@ -105,10 +92,12 @@ reg_selec   =  pn.widgets.Select(name='Regressor', options=['CO2EQ', 'EESC', 'EN
 
 per_selec   =  pn.widgets.RadioBoxGroup(name='Period', options=['1960-2099', '2011-2099', '1960-2010'], inline=True)
 
+var_selec   =  pn.widgets.RadioBoxGroup(name='Variable', options=['zmnoy', 'zmo3', 'zmta', 'zmua'], inline=True)
+
 
 #creating Quadmesh
 graph_opts = dict(cmap = 'RdBu_r', symmetric=True, logy = True, colorbar = True, \
-                ylim=(1000,0.1), active_tools=['pan'],title='Tap to compare ens')
+                ylim=(1000,0.1), active_tools=['pan'],title='Tap to compare ens', toolbar="below")
 
 graph = ds_sel['coefs'].hvplot.quadmesh(x = 'x', y = 'y' ).opts(**graph_opts)
 
@@ -122,37 +111,37 @@ taps_graph = hv.DynamicMap(
                 streams=[tap_stream])
 
 @pn.depends(stream.param.x, stream.param.y, month_sel=month_selec.param.value,
-            reg_sel=reg_selec.param.value, per_sel=per_selec.param.value, ens_sel=ens_selec.param.value)
-def location(x, y, month_sel, reg_sel, per_sel, ens_sel):
+            reg_sel=reg_selec.param.value, per_sel=per_selec.param.value,
+            ens_sel=ens_selec.param.value, var_sel=var_selec.param.value)
+def location(x, y, month_sel, reg_sel, per_sel, ens_sel, var_sel):
 
-    first_column = pn.pane.Str(f'Click at {x:.2f}, {y:.2f}, month = {month_sel:.0f} , reg = {reg_sel} , per = {per_sel}, ens ={ens_sel}')
+    first_column = pn.pane.Markdown(f'#### Click at {x:.2f}, {y:.2f} </br> <b>{var_sel}</b> response to <b>{reg_sel}</b> in <b>{month_names[month_sel-1]}</b></br> choosen period: <b>{per_sel}</b></br> choosen model: <b>{ens_names[ens_sel]}</b>', style={'font-family': "calibri",'color':"green"})
     hv_panel[1][0][0].value=per_sel
-    hv_panel[1][0][1].value=ens_sel
-    hv_panel[1][0][2].value=reg_sel
+    hv_panel[1][0][1].value=var_sel
+    hv_panel[1][0][2].value=ens_sel
     hv_panel[1][0][3].value=month_sel
+    hv_panel[1][0][4].value=reg_sel
+    box = pn.WidgetBox(var_selec, per_selec, reg_selec, month_selec, ens_selec,width=390)
     if np.nan not in [x,y]:
 
 
-        temp3=ds_sel.sel(month=month_sel, reg=reg_sel, per=per_sel)
+        temp3=ds_sel.sel(month=month_sel, reg=reg_sel, per=per_sel, var=var_sel)
         temp = temp3.sel(x=x,y=y, method = 'nearest')
         temp2 = temp['coefs'].where(temp['p_values'] < 0.05) # mark stat. sign. values
         second_column = temp['coefs'].hvplot(width = 400) * temp2.hvplot.scatter(c='k')
     else:
         second_column = pn.Spacer(name='Series Graph')
-    return pn.Column(first_column, second_column, month_selec, reg_selec, per_selec, ens_selec)
+    return pn.Column(first_column, second_column, box)
     
 #  adding panel to gain control over widgets
 hv_panel = pn.panel(graph*taps_graph)
-hv_panel.pprint()
+#hv_panel.pprint()
 
 # +
-gspec = pn.GridSpec(width=800, height=600)
+gspec = pn.GridSpec(width=800, height=600, margin=5)
 gspec[0, 0] = hv_panel[0]
 gspec[0, 1] = location
    
 gspec
 # -
-
-
-
 
